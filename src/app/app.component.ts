b7ce71ed6d10fae53f6daaa753e963910aa5f3cc
Preston Lamb <preston.j.lamb@gmail.com>
2021-04-23 09:11:59 -0600

    added demo content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: environment.useTailwindTemplate ? './app-tw.component.html' : './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'lightning-swap-templates';
+	title = 'lightning-swap-templates';
 }
